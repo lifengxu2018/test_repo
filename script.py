7352c06d3972585b7c447846b5c6360f67c38f5b
Lifeng Xu <lxu@rivian.com>
2019-12-24 14:46:41 -0800

    remove some code

--- script.py
+++ script.py
@@ -1,16 +1,16 @@
-import math
-from os import rename
-import sys
+# import math
+# from os import rename
+# import sys
 
 import requests
 
 # print(sys.version)
-print(sys.executable)
+# print(sys.executable)
 
 
-def greet(who_to_greet):
-    greeting = "hello, {}".format(who_to_greet)
-    return greeting
+# def greet(who_to_greet):
+#     greeting = "hello, {}".format(who_to_greet)
+#     return greeting
 
 
 # print(greet('World'))
